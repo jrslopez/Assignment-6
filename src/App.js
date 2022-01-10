3d19235140562adeaff6176950faaa345acdf602
James Lopez <lopez.jamesrussel@gmail.com>
2022-01-10 14:46:09 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
 
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+  // call API
+  const fetchPost = async () => {
+
+    /*
+    const response = await axios("http://ctp-zip-api.herokuapp.com/zip/10010")
+    console.log(response.data)
+    setPosts(response.data)
+    */
+
+    fetch("http://ctp-zip-api.herokuapp.com/zip/10010")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          setPosts(result)
+        }
+      )
+  }
+
+  useEffect(() => {
+    fetchPost()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <p>{posts.map(element => {
+        console.log(element.City)
+      })}</p>
+      <button onClick={fetchPost}>Click to get a new joke</button>
     </div>
   );
 }
