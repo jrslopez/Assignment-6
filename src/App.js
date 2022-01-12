28080111321c21cef65e25048b66dbb10dadef53
James Lopez <lopez.jamesrussel@gmail.com>
2022-01-12 13:34:12 -0500

    Search function working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios'
 
 
 function App() {
 
-  const [posts, setPosts] = useState([])
-  const zipCode = 10314
+  const [post, setPost] = useState([])
+  const [zip, setZip] = useState([])
 
   // call API
   const fetchPost = async () => {
   
-    const response = await axios("http://ctp-zip-api.herokuapp.com/zip/" + zipCode)
-    //const response = await axios("https://api.chucknorris.io/jokes/random")
-    console.log(response.data)
-    setPosts(response.data)
+    const response = await axios(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+    setPost(response.data)
   
   }
 
@@ -25,19 +23,22 @@ function App() {
     <div className="zip-search">
       <h1>Hello world</h1>
       <div>
-        {posts.map(element => {
-        return(<div className = "City" key={element.RecordNumber}>
+        <input type = "text" value = {zip} onChange = {e => setZip(e.target.value)}/>
+        <button onClick={fetchPost}>Search new city</button>
+      </div>
+      <div>
+        {post.map(element => {
+        return(
+            <div className = "City" key={element.RecordNumber}>
+            <div><br></br></div>
             <div>{element.City}</div>
             <div>State: {element.State}</div>
             <div>Location: {element.Location}</div>
             <div>Population: {element.EstimatedPopulation}</div>
             <div>Total Wages: {element.TotalWages}</div>
-            <div></div>
-
         </div>
         
         )})}</div>
-      <button onClick={fetchPost}>Search new city</button>
     </div>
   );
 
---
+++ src/FetchZip.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function FetchZip() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FetchZip
