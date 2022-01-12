d26ddcb5830b17e97a4c06077d88e9c6b669f55d
James Lopez <lopez.jamesrussel@gmail.com>
2022-01-11 19:04:47 -0500

    Added test display

--- src/App.js
+++ src/App.js
@@ -5,24 +5,16 @@ import axios from 'axios'
 function App() {
 
   const [posts, setPosts] = useState([])
+  const zipCode = 10314
 
   // call API
   const fetchPost = async () => {
-
-    /*
-    const response = await axios("http://ctp-zip-api.herokuapp.com/zip/10010")
+  
+    const response = await axios("http://ctp-zip-api.herokuapp.com/zip/" + zipCode)
+    //const response = await axios("https://api.chucknorris.io/jokes/random")
     console.log(response.data)
     setPosts(response.data)
-    */
-
-    fetch("http://ctp-zip-api.herokuapp.com/zip/10010")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result)
-          setPosts(result)
-        }
-      )
+  
   }
 
   useEffect(() => {
@@ -30,14 +22,32 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div className="zip-search">
       <h1>Hello world</h1>
-      <p>{posts.map(element => {
-        console.log(element.City)
-      })}</p>
-      <button onClick={fetchPost}>Click to get a new joke</button>
+      <div>
+        {posts.map(element => {
+        return(<div className = "City" key={element.RecordNumber}>
+            <div>{element.City}</div>
+            <div>State: {element.State}</div>
+            <div>Location: {element.Location}</div>
+            <div>Population: {element.EstimatedPopulation}</div>
+            <div>Total Wages: {element.TotalWages}</div>
+            <div></div>
+
+        </div>
+        
+        )})}</div>
+      <button onClick={fetchPost}>Search new city</button>
     </div>
   );
+
+  /*return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>{posts.value}</p>
+      <button onClick={fetchPost}>Click to get a new joke</button>
+    </div>
+  );*/
 }
 
 export default App;
